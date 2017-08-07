cbdc6d19548ddb7ce7d356b78ccf0b432a90f342
Vladimir Scherbak <vscherbak@newagesol.com>
2017-08-07 16:09:05 +0300

    style

--- src/components/ArtistCard/index.js
+++ src/components/ArtistCard/index.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
-//import './style.scss'
+import './style.scss'
 
 class ArtistCard extends Component {
 	render() {
 		const item = this.props.item
 		let linkName = '/artist/'+item.name.replace(' ', '_');
 		return (
-			<div className='col-md-3'>
+			<div className='col-xs-4 col-sm-4 col-md-4 col-lg-4'>
 				<Link to={linkName}>
-					<div className='media'>
-						<div className='media-left'>
-								<img className='media-object' src={item.image[0]['#text']} alt={item.name} />
-						</div>
-						<div className='media-body'>
-							<h4 className='media-heading'>{item.name}</h4>
+					<div className='thumbnail'>
+						<img src={item.image[2]['#text']} alt={item.name} />
+						<div className='caption'>
+							<h3>{item.name}</h3>
 						</div>
 					</div>
 				</Link>
